200559e5712ab1da31dce13a7ac588229c4e05c4
Jesse van Oostrum <jessevoostrum@gmail.com>
2022-03-18 16:27:01 +0100

    global y coordinates for barlines

--- PlotterBarlines.py
+++ PlotterBarlines.py
@@ -12,8 +12,9 @@ class PlotterBarlines:
         self.settings = settings
 
         self.LocationFinder = LocationFinder
+        self.CanvasCreator = CanvasCreator
 
-        self.axs1D = CanvasCreator.getAxs()
+        self.axs = CanvasCreator.getAxs()
         self.widthAxs = CanvasCreator.getWidthAx()
 
         self.yMin = yMin
@@ -30,15 +31,20 @@ class PlotterBarlines:
         linewidth_2 = self.settings["linewidth_2"]
 
         for measure in self.streamObj.recurse().getElementsByClass(music21.stream.Measure):
+            line = self.LocationFinder.getLocation(measure.offset)[0]  #this is assuming the whole measure stays on the same line
+            page = self.CanvasCreator.getLinesToPage()[line]
+            yPosLineBase = self.CanvasCreator.getYPosLineBase(line)
+            yPosLow = yPosLineBase + self.yMin
+            yPosHigh = yPosLineBase + self.yMax
             if not measure.number == 0:
                 # vline start measure
                 line, xPos = self.LocationFinder.getLocation(measure.offset)
-                self.axs1D[line].vlines(xPos, self.yMin, self.yMax + heightBarline0Extension,
+                self.axs[page].vlines(xPos, yPosLow, yPosHigh + heightBarline0Extension,
                                         linestyle='solid', linewidth=linewidth_0, color='grey', zorder=.5)
             # vline end measure
             offsetEndMeasure = measure.offset + measure.quarterLength
             line, xPos = self.LocationFinder.getLocation(offsetEndMeasure, start=False)
-            self.axs1D[line].vlines(xPos, self.yMin, self.yMax + heightBarline0Extension,
+            self.axs[page].vlines(xPos, yPosLow, yPosHigh + heightBarline0Extension,
                                     linestyle='solid', linewidth=linewidth_0, color='grey', zorder=.5)
 
             if not measure.number == 0:
@@ -46,14 +52,14 @@ class PlotterBarlines:
                     for t in np.arange(0, measure.quarterLength, step=1):
                         offset = measure.offset + t
                         line, xPos = self.LocationFinder.getLocation(offset, start=True)
-                        self.axs1D[line].vlines(xPos, self.yMin, self.yMax,
+                        self.axs[page].vlines(xPos, yPosLow, yPosHigh,
                                 linestyle='solid', linewidth=linewidth_1, color='grey', zorder=.5)
 
                 if self.subdivision >= 2:
                     for t in np.arange(0, measure.quarterLength, step=.25):
                         offset = measure.offset + t
                         line, xPos = self.LocationFinder.getLocation(offset, start=True)
-                        self.axs1D[line].vlines(xPos, self.yMin, self.yMax,
+                        self.axs[page].vlines(xPos, yPosLow, yPosHigh,
                                                 linestyle='solid', linewidth=linewidth_2, color='grey', zorder=.5)
 
             if measure.number == 0:
@@ -61,13 +67,13 @@ class PlotterBarlines:
                      for t in np.arange(0, measure.quarterLength, step=1):
                          offset = measure.offset + measure.quarterLength - t
                          line, xPos = self.LocationFinder.getLocation(offset, start=True)
-                         self.axs1D[line].vlines(xPos, self.yMin, self.yMax,
+                         self.axs[page].vlines(xPos, yPosLow, yPosHigh,
                                                  linestyle='solid', linewidth=linewidth_1, color='grey', zorder=.5)
 
                  if self.subdivision >= 2:
                      for t in np.arange(0, measure.quarterLength, step=.25):
                          offset = measure.offset + measure.quarterLength - t
                          line, xPos = self.LocationFinder.getLocation(offset, start=True)
-                         self.axs1D[line].vlines(xPos, self.yMin, self.yMax,
+                         self.axs[page].vlines(xPos, yPosLow, yPosHigh,
                                                  linestyle='solid', linewidth=linewidth_2, color='grey', zorder=.5)
 
