092157fed8d970d7207dedb712b9d6574765f31c
Jesse van Oostrum <jessevoostrum@gmail.com>
2022-03-19 10:11:12 +0100

    change x coordinates in plotter notes

--- Visualiser.py
+++ Visualiser.py
@@ -64,7 +64,7 @@ class Visualiser:
                                self.CanvasCreator.getTitleAx(), key)
     def plot(self):
 
-        # self.PlotterNotes.plotNotes()
+        self.PlotterNotes.plotNotes()
         self.PlotterChords.plotChords()
         self.PlotterBarlines.plotBarlines()
         self.PlotterMetadata.plotMetadata()
