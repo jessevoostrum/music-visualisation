59574ac7f3e0a7d306eaf940582552472dfc7987
Jesse van Oostrum <jessevoostrum@gmail.com>
2022-03-20 17:43:52 +0100

    cleanup setting names in plotter notes

--- PlotterNotes.py
+++ PlotterNotes.py
@@ -19,15 +19,11 @@ class PlotterNotes:
         self.linesToPage = CanvasCreator.getLinesToPage()
 
         self.barSpace = settings["barSpace"]
-        self.overlapFactor = settings["overlapFactor"]
         self.noteLowest = noteLowest
 
         self.alpha = settings["alpha"]
         self.facecolor = settings["facecolor"]
 
-        self.yShiftNumbers = settings['yShiftNumbers']
-        self.xShiftNumbersFixed = settings['xShiftNumbersPerWidthFixed']
-        self.xWidthNumber = settings["widthNumber"]
 
         self.xExtensionNoteWhenTied = settings["radiusCorners"] + settings["xPadding"]
 
@@ -45,17 +41,14 @@ class PlotterNotes:
 
                 yPosLineBase = self.CanvasCreator.getYPosLineBase(line)
 
-                yPosWithinLine = (el.pitch.ps - self.noteLowest) * self.barSpace * (1 - self.overlapFactor)
+                yPosWithinLine = (el.pitch.ps - self.noteLowest) * self.barSpace * (1 - self.settings["overlapFactor"])
 
                 yPos = yPosLineBase + yPosWithinLine
 
                 page = self.linesToPage[line]
 
                 offsetLength = el.duration.quarterLength  # - 2 * self.xExtensionNoteWhenTied
-                facecolor = self.facecolor
-                linewidth = 0
-                alpha = self.alpha
-                hatch = None
+
 
 
                 xPos = self.CanvasCreator.getXPosFromOffsetLine(offsetLine)
@@ -78,24 +71,21 @@ class PlotterNotes:
                         xLength += self.xExtensionNoteWhenTied
                         xPos -= self.xExtensionNoteWhenTied
 
+                facecolor = self.facecolor
+                alpha = self.alpha
+                hatch = None
                 # ghost note
-                ghostNote = False
                 if el.notehead == 'x':
-                    ghostNote = True
                     facecolor = 'grey'
-                    linewidth = 0
                     alpha = 0.15
                     hatch = 'xxxxx'   # this controls the fine graindedness of the x pattern? TODO: make notebook
-
-
-                
 
                 patch = FancyBboxPatch((xPos, yPos),
                                        xLength, self.barSpace,
                                        boxstyle=f"round, pad=0, rounding_size={self.settings['radiusCorners']}",
                                        mutation_aspect=self.settings["mutationAspect"],
                                        fc=facecolor, alpha=alpha,
-                                       edgecolor='black', linewidth=linewidth,
+                                       edgecolor='black', linewidth=0,
                                        hatch=hatch
                                        )
 
@@ -103,26 +93,29 @@ class PlotterNotes:
                 self.axs[page].add_patch(rec)
                 patch.set_clip_path(rec)
 
+                self.plotNumber(el, page, xLengthBeforeExtension, xPos, yPos)
 
-                # plot numbers
-                if not (el.tie and not (el.tie.type == 'start')):
-                    number, accidental = self.key.getScaleDegreeAndAccidentalFromPitch(el.pitch)
-                    if accidental:
-                        if accidental.name == 'sharp':
-                            number = f"$ {{}}^\\# {number}$"
-                        elif accidental.name == 'flat':
-                            number = f"$ {{}}^b {number}$"
-                    else:
-                        number = f"${number}$"
 
-                    xCenterNumbers = 0.015625
-                    xShiftNumbers = xCenterNumbers + 0.5 * self.xWidthNumber
-                    if xLengthBeforeExtension < (2 * xCenterNumbers):
-                        xShiftNumbers = 0.5 * xLengthBeforeExtension + 0.5 * self.xWidthNumber
 
-                    self.axs[page].text(xPos + xShiftNumbers,
-                                          yPos + self.yShiftNumbers + 0.5 * self.barSpace,
-                                          number, fontsize=self.settings['fontSizeNotes'],
-                                          va='center', ha='right')
+    def plotNumber(self, el, page, xLengthBeforeExtension, xPos, yPos):
+        if not (el.tie and not (el.tie.type == 'start')):
+            number, accidental = self.key.getScaleDegreeAndAccidentalFromPitch(el.pitch)
+            if accidental:
+                if accidental.name == 'sharp':
+                    number = f"$ {{}}^\\# {number}$"
+                elif accidental.name == 'flat':
+                    number = f"$ {{}}^b {number}$"
+            else:
+                number = f"${number}$"
+
+            xShiftNumbers = self.settings["xShiftCenterOfNumber"] + 0.5 * self.settings["xWidthNumber"]
+            if xLengthBeforeExtension < (2 * self.settings["xShiftCenterOfNumber"]):
+                xShiftNumbers = 0.5 * xLengthBeforeExtension + 0.5 * self.settings["xWidthNumber"]
+
+            self.axs[page].text(xPos + xShiftNumbers,
+                                yPos + self.settings["yShiftNumbers"] + 0.5 * self.barSpace,
+                                number, fontsize=self.settings['fontSizeNotes'],
+                                va='center', ha='right')
+
 
 
