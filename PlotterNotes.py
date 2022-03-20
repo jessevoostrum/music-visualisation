581b76253d9274bc509aea489ad4207337f7760a
Jesse van Oostrum <jessevoostrum@gmail.com>
2022-03-20 18:06:36 +0100

    refactor plotter notes

--- PlotterNotes.py
+++ PlotterNotes.py
@@ -24,9 +24,6 @@ class PlotterNotes:
         self.alpha = settings["alpha"]
         self.facecolor = settings["facecolor"]
 
-
-        self.xExtensionNoteWhenTied = settings["radiusCorners"] + settings["xPadding"]
-
         self.key = key
 
     def plotNotes(self):
@@ -38,64 +35,38 @@ class PlotterNotes:
 
                 offset = el.getOffsetInHierarchy(self.streamObj)  # + self.xExtensionNoteWhenTied
                 line, offsetLine = self.LocationFinder.getLocation(offset)
-
                 yPosLineBase = self.CanvasCreator.getYPosLineBase(line)
-
                 yPosWithinLine = (el.pitch.ps - self.noteLowest) * self.barSpace * (1 - self.settings["overlapFactor"])
-
                 yPos = yPosLineBase + yPosWithinLine
 
                 page = self.linesToPage[line]
 
                 offsetLength = el.duration.quarterLength  # - 2 * self.xExtensionNoteWhenTied
 
-
-
                 xPos = self.CanvasCreator.getXPosFromOffsetLine(offsetLine)
                 xLength = self.CanvasCreator.getXLengthFromOffsetLength(offsetLength)
 
+                self.plotRectangle(el, page, xLength, xPos, yPos)
 
-                rec = Rectangle((xPos, yPos), xLength, self.barSpace, facecolor="none", edgecolor="none")
-                xLengthBeforeExtension = xLength
+                self.plotNumber(el, page, xLength, xPos, yPos)
 
-                xPos += self.settings["xPadding"]
-                xLength -= 2 * self.settings["xPadding"]
-
-                if el.tie:
-                    if el.tie.type == 'start':
-                        xLength += self.xExtensionNoteWhenTied
-                    elif el.tie.type == 'continue':
-                        xLength += 2 * self.xExtensionNoteWhenTied
-                        xPos -= self.xExtensionNoteWhenTied
-                    elif el.tie.type == 'stop':
-                        xLength += self.xExtensionNoteWhenTied
-                        xPos -= self.xExtensionNoteWhenTied
-
-                facecolor = self.facecolor
-                alpha = self.alpha
-                hatch = None
-                # ghost note
-                if el.notehead == 'x':
-                    facecolor = 'grey'
-                    alpha = 0.15
-                    hatch = 'xxxxx'   # this controls the fine graindedness of the x pattern? TODO: make notebook
-
-                patch = FancyBboxPatch((xPos, yPos),
-                                       xLength, self.barSpace,
-                                       boxstyle=f"round, pad=0, rounding_size={self.settings['radiusCorners']}",
-                                       mutation_aspect=self.settings["mutationAspect"],
-                                       fc=facecolor, alpha=alpha,
-                                       edgecolor='black', linewidth=0,
-                                       hatch=hatch
-                                       )
-
-                self.axs[page].add_patch(patch)
-                self.axs[page].add_patch(rec)
-                patch.set_clip_path(rec)
-
-                self.plotNumber(el, page, xLengthBeforeExtension, xPos, yPos)
-
-
+    def plotRectangle(self, el, page, xLength, xPos, yPos):
+        xPos += self.settings["xMarginNote"]
+        xLength -= 2 * self.settings["xMarginNote"]
+        rec = Rectangle((xPos, yPos), xLength, self.barSpace, facecolor="none", edgecolor="none")
+        xLength, xPos = self.extendNotesWhenTied(el, xLength, xPos)
+        alpha, facecolor, hatch = self.adjustVisualParametersForGhostNote(el)
+        patch = FancyBboxPatch((xPos, yPos),
+                               xLength, self.barSpace,
+                               boxstyle=f"round, pad=0, rounding_size={self.settings['radiusCorners']}",
+                               mutation_aspect=self.settings["mutationAspect"],
+                               fc=facecolor, alpha=alpha,
+                               edgecolor='black', linewidth=0,
+                               hatch=hatch
+                               )
+        self.axs[page].add_patch(patch)
+        self.axs[page].add_patch(rec)
+        patch.set_clip_path(rec)
 
     def plotNumber(self, el, page, xLengthBeforeExtension, xPos, yPos):
         if not (el.tie and not (el.tie.type == 'start')):
@@ -116,6 +87,32 @@ class PlotterNotes:
                                 yPos + self.settings["yShiftNumbers"] + 0.5 * self.barSpace,
                                 number, fontsize=self.settings['fontSizeNotes'],
                                 va='center', ha='right')
+
+
+    def adjustVisualParametersForGhostNote(self, el):
+        facecolor = self.facecolor
+        alpha = self.alpha
+        hatch = None
+        # ghost note
+        if el.notehead == 'x':
+            facecolor = 'grey'
+            alpha = 0.15
+            hatch = 'xxxxx'  # this controls the fine graindedness of the x pattern? TODO: make notebook
+        return alpha, facecolor, hatch
+
+    def extendNotesWhenTied(self, el, xLength, xPos):
+        xExtensionNoteWhenTied = self.settings["radiusCorners"] + self.settings["xMarginNote"]
+        if el.tie:
+            if el.tie.type == 'start':
+                xLength += xExtensionNoteWhenTied
+            elif el.tie.type == 'continue':
+                xLength += 2 * xExtensionNoteWhenTied
+                xPos -= xExtensionNoteWhenTied
+            elif el.tie.type == 'stop':
+                xLength += xExtensionNoteWhenTied
+                xPos -= xExtensionNoteWhenTied
+        return xLength, xPos
+
 
 
 
