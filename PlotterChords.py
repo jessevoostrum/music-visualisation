491f57ad13856affef735c20e7a4aaea4ebe8371
Jesse van Oostrum <jessevoostrum@gmail.com>
2022-04-23 11:43:15 +0100

    add addition to chord plotting

--- PlotterChords.py
+++ PlotterChords.py
@@ -32,8 +32,10 @@ class PlotterChords(Plotter):
 
             self._plotChordNumberAndAccidental(chord, xPos, yPos, page)
 
-            # self.axs[page].text(xPos, yPos, self._findFigure(chord),
-            #                       va='bottom', size=self.settings['fontSizeChords'], fontweight='semibold')
+            lengthAddition = self._plotAddition(chord, xPos, yPos, page)
+
+            self._plotBass(chord, xPos, yPos, page, lengthAddition)
+
 
     def _plotChordNumberAndAccidental(self, chordSymbol, xPos, yPos, page):
         number, accidental = self.key.getScaleDegreeAndAccidentalFromPitch(chordSymbol.root())
@@ -43,6 +45,60 @@ class PlotterChords(Plotter):
 
         self.plotAccidental(accidental, self.settings['fontSizeChords'], xPos, yPos, page)
 
+
+    def _plotAddition(self, chordSymbol, xPos, yPos, page):
+
+        addition = self._findAddition(chordSymbol)
+
+        # lengthAddition = len(addition)  # does not work
+
+        fontSize = self.settings['fontSizeChords']
+
+        self.axs[page].text(xPos + (1 + self.settings["hDistanceChordAddition"]) * self.settings["widthNumberRelative"] * fontSize,
+                            yPos + self.settings['capsizeNumberRelative'] * fontSize * 0.7, addition,
+                            fontsize=self.settings['fontSizeAccidentalRelative'] * fontSize,
+                            va='baseline', ha='left',
+                            # fontname='Arial',
+                            fontweight=1)
+
+        # return lengthAddition
+
+    def _findAddition(self, chordSymbol):
+        addition = ""
+
+        kind = chordSymbol.chordKind
+
+        if kind in music21.harmony.CHORD_ALIASES:
+            kind = music21.harmony.CHORD_ALIASES[kind]
+
+        if kind in music21.harmony.CHORD_TYPES:
+            addition += chordTypes.getAbbreviationListGivenChordType(kind)[0]
+
+        for csMod in chordSymbol.chordStepModifications:
+            if csMod.interval is not None:
+                numAlter = csMod.interval.semitones
+                if numAlter > 0:
+                    s = '\\#\!'
+                else:
+                    s = 'b'
+                prefix = s * abs(numAlter)
+                if abs(numAlter) > 0:
+                    prefix = '\,{{}}^' + prefix
+
+                # print("numAlter", numAlter, "\n", "prefix", prefix)
+
+                # addition += ' ' + csMod.modType + ' ' + prefix + str(csMod.degree)
+                addition += prefix + str(csMod.degree)
+            else:
+                # addition += ' ' + csMod.modType + ' ' + str(csMod.degree)
+                addition += str(csMod.degree)
+
+        addition  = "$" + addition + "$"
+
+        return addition
+
+    def _plotBass(self, chord, xPos, yPos, page, lengthAddition):
+        pass
 
     def _findFigure(self, chordSymbol):
 
