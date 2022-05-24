e00387eb3d87a9caad9b999a1b0329d10c8394fc
Jesse van Oostrum <jessevoostrum@gmail.com>
2022-05-25 00:13:14 +0200

    create integer book

--- runMultiple.py
+++ runMultiple.py
@@ -5,10 +5,13 @@ from matplotlib.backends.backend_pdf import PdfPages
 
 from Visualiser import Visualiser
 
-dir_notes = '/Users/jvo/Library/Mobile Documents/com~apple~CloudDocs/projects/music_software/IFR_animation/notes/'
+# dir_notes = '/Users/jvo/Library/Mobile Documents/com~apple~CloudDocs/projects/music_software/IFR_animation/notes/'
+dir_notes = 'notes/'
 
 filename_bass = 'sample_songs_bass.txt'
-filename_standards = 'sample_songs.txt'
+# filename_standards = 'sample_songs.txt'
+filename_standards = 'standards.txt'
+
 
 dir_songs_bass = "/Users/jvo/Library/Mobile Documents/com~apple~CloudDocs/bladmuziek/bass_lines_SBL/"
 dir_songs_standards = "/Users/jvo/Library/Mobile Documents/com~apple~CloudDocs/bladmuziek/standards_musescore/"
@@ -40,4 +43,4 @@ for line in lines:
     streamObj = music21.converter.parse(path_songs + line)
 
     vis = Visualiser(streamObj, settings)
-    vis.generate("output/")
+    vis.generate("output-standards/")
