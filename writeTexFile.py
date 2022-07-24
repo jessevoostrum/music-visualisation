9bf8d4bb5742d248028ca7089dbda5678b7d2071
Jesse van Oostrum <jessevoostrum@gmail.com>
2022-07-24 20:05:50 +0100

    fix removal of suffix and change importing of titles

--- writeTexFile.py
+++ writeTexFile.py
@@ -1,10 +1,16 @@
 from PyPDF2 import PdfReader
 from string import Template
+import glob
+import os
 
 dirStandards = '/Users/jvo/Documents/music-visualisation/output-standards/'
 
-with open('notes/standards-pdf.txt', 'r') as reader:
-    lines = reader.readlines()
+# with open('notes/standards-pdf.txt', 'r') as reader:
+#     lines = reader.readlines()
+
+lines = glob.glob(dirStandards + '*' + '.pdf')
+lines = [os.path.basename(line) for line in lines]
+lines.sort()
 
 s0 = Template('\includepdf[pages= $numPage,  pagecommand={\\vspace*{-4.2cm}\phantomsection\\addcontentsline{toc}{section}{$letter} \\vspace*{-4.2cm}\phantomsection\\addcontentsline{toc}{subsection}{$title}}]{$path}')
 s1 = Template('\includepdf[pages= $numPage,  pagecommand={\\vspace*{-4.2cm}\phantomsection\\addcontentsline{toc}{subsection}{$title}}]{$path}')
@@ -12,11 +18,11 @@ s2 = Template('\includepdf[pages= $numPage, pagecommand={}]{$path}')
 
 with open('inputTexFile.txt', 'w') as writer:
 
-    firstLetter = 'z'
+    firstLetter = '_'
     for line in lines:
         line = line.rstrip('\n')
-        titleSong = line.rstrip('.pdf')
         pathSong = dirStandards + line
+        titleSong = line.removesuffix('.pdf')
         if titleSong[0] != firstLetter:
             firstLetter = titleSong[0]
             stringFirst = s0.substitute(numPage=1, letter=firstLetter, title=titleSong, path=pathSong)
@@ -30,5 +36,4 @@ with open('inputTexFile.txt', 'w') as writer:
         if numPages > 1:
             stringSecond = s2.substitute(numPage='2-', path=pathSong)
             writer.write(f'{stringSecond}\n')
-
 
