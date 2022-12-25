26abfec70d41f39d87b8d65e86a8c6c98a6de24d
Jesse van Oostrum <jessevoostrum@gmail.com>
2022-12-25 13:30:04 +0100

    rounded corners for parallelogram

--- sample/plotter/patches.py
+++ sample/plotter/patches.py
@@ -3,23 +3,32 @@ import matplotlib.patches as patches
 import numpy as np
 
 
-vDist = 0.0015  # TODO(make dependent on barSpace)
-hDist = 0.004
-vShift = np.array([0, vDist])
-hShift = np.array([hDist, 0])
-vShiftAux = np.array([0, 0.4 * vDist])
-hShiftAux = np.array([0.5 * hDist, 0])
+def Parallelogram(leftBottom, leftTop, rightBottom, rightTop, alpha=1, facecolor='blue', hatch=None, shape='straight'):
 
+    deltaX = rightBottom[0] - leftBottom[0]
+    deltaY = rightBottom[1] - leftBottom[1]
+    if deltaX > 0:
+        slope = deltaY / deltaX
+    else:
+        slope = 0
 
-def Parallelogram(leftBottom, leftTop, rightBottom, rightTop, alpha, facecolor, hatch, shape='straight'):
+    vDist = 0.0015  # TODO(make dependent on barSpace)
+    vDist = min(vDist, leftTop[1] - leftBottom[1])
+    hDist = 0.004
+    hDist = min(hDist, rightBottom[0] - leftBottom[0])
+    vShift = np.array([0, vDist])
+    hShift = np.array([hDist, 0]) + np.array([0, hDist * slope])
+    vShiftAux = 0.4 * vShift
+    hShiftAux = 0.5 * hShift
+
     if shape == 'rounded':
-        path = RoundedPath(leftBottom, leftTop, rightBottom, rightTop)
+        path = RoundedPath(leftBottom, leftTop, rightBottom, rightTop, vShift, hShift, vShiftAux, hShiftAux)
     elif shape == 'leftRounded':
-        path = LeftRoundedPath(leftBottom, leftTop, rightBottom, rightTop)
+        path = LeftRoundedPath(leftBottom, leftTop, rightBottom, rightTop, vShift, hShift, vShiftAux, hShiftAux)
     elif shape == 'rightRounded':
-        path = RightRoundedPath(leftBottom, leftTop, rightBottom, rightTop)
+        path = RightRoundedPath(leftBottom, leftTop, rightBottom, rightTop, vShift, hShift, vShiftAux, hShiftAux)
     elif shape == 'straight':
-        path = StraightPath(leftBottom, leftTop, rightBottom, rightTop)
+        path = StraightPath(leftBottom, leftTop, rightBottom, rightTop, vShift, hShift, vShiftAux, hShiftAux)
     else:
         print("undefined shape")
         return
@@ -28,7 +37,7 @@ def Parallelogram(leftBottom, leftTop, rightBottom, rightTop, alpha, facecolor, 
     return patch
 
 
-def RoundedPath(leftBottom, leftTop, rightBottom, rightTop):
+def RoundedPath(leftBottom, leftTop, rightBottom, rightTop, vShift, hShift, vShiftAux, hShiftAux):
     vertsBase = [leftBottom + vShift,
                  leftTop - vShift,
                  leftTop - vShiftAux,
@@ -71,7 +80,7 @@ def RoundedPath(leftBottom, leftTop, rightBottom, rightTop):
     path = Path(verts, codes)
     return path
 
-def LeftRoundedPath(leftBottom, leftTop, rightBottom, rightTop):
+def LeftRoundedPath(leftBottom, leftTop, rightBottom, rightTop, vShift, hShift, vShiftAux, hShiftAux):
     vertsBase = [leftBottom + vShift,
                  leftTop - vShift,
                  leftTop - vShiftAux,
@@ -102,7 +111,7 @@ def LeftRoundedPath(leftBottom, leftTop, rightBottom, rightTop):
     path = Path(verts, codes)
     return path
 
-def RightRoundedPath(leftBottom, leftTop, rightBottom, rightTop):
+def RightRoundedPath(leftBottom, leftTop, rightBottom, rightTop, vShift, hShift, vShiftAux, hShiftAux):
 
     vertsBase = [leftBottom,
                  leftTop,
@@ -135,7 +144,7 @@ def RightRoundedPath(leftBottom, leftTop, rightBottom, rightTop):
     path = Path(verts, codes)
     return path
 
-def StraightPath(leftBottom, leftTop, rightBottom, rightTop):
+def StraightPath(leftBottom, leftTop, rightBottom, rightTop, vShift, hShift, vShiftAux, hShiftAux ):
 
     vertsBase = [leftBottom,
                  leftTop,
