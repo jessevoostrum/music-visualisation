f352d9e0fa884f1f7a0a45332e6473d04dc76814
Jesse van Oostrum <jessevoostrum@gmail.com>
2023-01-09 12:11:54 +0100

    create testsuite files and remove bass staff

--- sample/main.py
+++ sample/main.py
@@ -1,4 +1,5 @@
 """Jesse van Oostrum"""
+import music21.stream
 
 from sample.plotter.PlotterMain import PlotterMain
 from sample.LocationFinder import LocationFinder
@@ -10,7 +11,7 @@ class Visualiser:
     """class for making visualisations from a music21 stream"""
 
     def __init__(self, streamObj, settings):
-        self.streamObj = streamObj
+        self.streamObj = self._preprocessStreamObj(streamObj)
 
         self.Settings = Settings(streamObj, settings)
 
@@ -28,6 +29,19 @@ class Visualiser:
         yPosLowest = self.LocationFinder.getYPosLineBase(-1)
 
         self.CanvasCreator.saveFig(title, pathName, yPosLowest)
+
+    def _preprocessStreamObj(self, streamObj):
+
+        streamObj = self._removeBassStaff(streamObj)
+
+        return streamObj
+
+    def _removeBassStaff(self, streamObj):
+        staffs = streamObj[music21.stream.PartStaff]
+        if staffs:
+            if len(staffs) > 1:
+                streamObj.remove(staffs[1])
+        return streamObj
 
 
 if __name__ == '__main__':
