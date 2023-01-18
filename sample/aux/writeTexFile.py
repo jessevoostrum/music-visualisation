495af6982d92893ae13dbd1300bf86eeef79c5e3
Jesse van Oostrum <jessevoostrum@gmail.com>
2023-01-18 22:43:17 +0100

    replace input tex file

--- sample/aux/writeTexFile.py
+++ sample/aux/writeTexFile.py
@@ -3,7 +3,7 @@ from string import Template
 import glob
 import os
 
-dirStandards = '/Users/jvo/Documents/music-visualisation/output-standards/'
+dirStandards = '/Users/jvo/Documents/music-visualisation/output/output-standards/'
 
 lines = glob.glob(dirStandards + '*' + '.pdf')
 lines = [os.path.basename(line) for line in lines]
@@ -16,7 +16,7 @@ s0 = Template('\includepdf[pages= $numPage,  pagecommand={\\vspace*{-4.2cm}\phan
 s1 = Template('\includepdf[pages= $numPage,  pagecommand={\\vspace*{-4.2cm}\phantomsection\\addcontentsline{toc}{subsection}{$title}}]{$path}')
 s2 = Template('\includepdf[pages= $numPage, pagecommand={}]{$path}')
 
-with open('../../inputTexFile.txt', 'w') as writer:
+with open('inputTexFile.txt', 'w') as writer:
 
     firstLetter = '_'
     for line in lines:
