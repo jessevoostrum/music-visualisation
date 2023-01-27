64a0e47d3af98319af962a2729817f09abe70a94
Jesse van Oostrum <jessevoostrum@gmail.com>
2023-01-27 09:10:00 +0100

    add elaborate example

---
+++ example/elaborateExample.py
@@ -0,0 +1,16 @@
+import json
+
+from sample.main import Visualiser
+
+f = open('../sample/settings.json')
+settings = json.load(f)
+
+settings["measuresPerLine"] = 2
+settings["subdivision"] = 2
+settings['thickBarlines'] = False
+
+pathToSong = "Express Yourself.mxl"
+
+vis = Visualiser(pathToSong, settings)
+vis.generate()
+
