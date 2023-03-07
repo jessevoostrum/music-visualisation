ef467e398d3f22336ca02a9d1b85cb553c5906c3
Jesse van Oostrum <jessevoostrum@gmail.com>
2023-03-07 18:34:08 +0100

    realbook font in cli

--- run.py
+++ run.py
@@ -1,3 +1,4 @@
+import os
 import json
 import argparse
 
@@ -6,10 +7,12 @@ from sample.main import Visualiser
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument("pathSong")
+parser.add_argument("pathSong", nargs='?', default="example/All_Of_Me.mxl")
 parser.add_argument("dirOutput")
 
 parser.add_argument("-b", "--bass", action="store_true")
+parser.add_argument("-r", "--realbookFont", action="store_true")
+
 
 args = parser.parse_args()
 
@@ -22,7 +25,11 @@ if args.bass:
     settings["subdivision"] = 2
     settings['thickBarlines'] = False
 
+if args.realbookFont:
+    settings['fontDirectory'] = "sample/fonts/Realbook"
+    settings['font'] = 'Realbook'
+
 vis = Visualiser(args.pathSong, settings)
-vis.generate(args.dirOutput)
+vis.generate(args.dirOutput + '/')
 
 
